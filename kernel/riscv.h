f036c8f4a3f80e9a0e267024a8f7bf504a96b0aa
ljy <952984167@qq.com>
2024-08-09 06:25:31 -0400

    lab4.2

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -319,6 +319,14 @@ sfence_vma()
   asm volatile("sfence.vma zero, zero");
 }
 
+//将当前执行函数的帧指针fp存储在寄存器s0中
+static inline uint64
+r_fp()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  return x;
+}
 
 #define PGSIZE 4096 // bytes per page
 #define PGSHIFT 12  // bits of offset within a page
