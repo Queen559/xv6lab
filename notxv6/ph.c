e671c8faf35a62cb70a7d23040d0c8914a52a32a
ljy <952984167@qq.com>
2024-08-16 06:22:56 -0400

    Lab7.2完成

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,8 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
+
 
 double
 now()
@@ -39,7 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+   pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -60,12 +63,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&lock[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&lock[i]);
   return e;
 }
 
@@ -114,7 +117,7 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  for (int i=0; i<NBUCKET; i++) pthread_mutex_init(&lock[i], NULL);
   //
   // first the puts
   //
