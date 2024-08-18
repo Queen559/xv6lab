e3cdea5c0b327fbedfe375bc08cffd3526d02da6
ljy <952984167@qq.com>
2024-08-18 11:09:50 -0400

    Lab8完成

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,13 @@
 struct buf {
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
+  uint dev;    //设备号
+  uint blockno; //块号
   struct sleeplock lock;
   uint refcnt;
   struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
+  uint timestamp;  // 时间戳
 };
 
